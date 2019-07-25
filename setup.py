bee1c2ce034b802aacab785f1b0deeac172163e3
Michael S. P. Kelley <msk@astro.umd.edu>
2019-07-25 12:38:53 -0400

    v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 if __name__ == "__main__":
     setup(name='sbsearch',
-          version='0.1.0',
+          version='0.1.2',
           description='Find small Solar System bodies in astronomical surveys.',
           author="Michael S. P. Kelley",
           author_email="msk@astro.umd.edu",
