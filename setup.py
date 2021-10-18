e9e84ee8e292725dd491b32799e3099a568d1337
Michael S. P. Kelley <msk@astro.umd.edu>
2021-10-18 16:18:19 -0400

    Requirements bump.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(name='sbsearch',
-          version='1.1.5',
+          version='1.1.6',
           description='Find small Solar System bodies in astronomical surveys.',
           author="Michael S. P. Kelley",
           author_email="msk@astro.umd.edu",
           url="https://github.com/mkelley/sbsearch",
           packages=find_packages(),
-          install_requires=['numpy>=1.13', 'astropy<4.0', 'astroquery>=0.4.dev5744', 'sbpy>=0.2.2',
+          install_requires=['numpy>=1.17', 'astropy>4', 'astroquery>=0.4.4.dev7007', 'sbpy>0.2.2',
                             'sqlalchemy>=1.3', 'geoalchemy2'],
           setup_requires=['pytest-runner'],
           tests_require=['pytest'],
