95a6c5c2328653aca78af971bbfbf61282d83dc7
Michael S. P. Kelley <msk@astro.umd.edu>
2022-01-21 09:27:12 -0500

    Pin to astropy 4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
           author_email="msk@astro.umd.edu",
           url="https://github.com/mkelley/sbsearch",
           packages=find_packages(),
-          install_requires=['numpy>=1.17', 'astropy>4', 'astroquery>=0.4.4.dev7007', 'sbpy>0.2.2',
+          install_requires=['numpy>=1.17', 'astropy>4<5', 'astroquery>=0.4.4.dev7007', 'sbpy>0.2.2',
                             'sqlalchemy>=1.3', 'geoalchemy2'],
           setup_requires=['pytest-runner'],
           tests_require=['pytest'],
