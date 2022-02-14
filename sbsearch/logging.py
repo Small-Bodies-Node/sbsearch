55a088afe7fcb08f98ef5879017f879d483b7605
Michael S. P. Kelley <msk@astro.umd.edu>
2022-02-14 15:06:36 -0500

    Update logging
    Restore elapsed time formatter.
    
    Fix ProgressWidget class (was coded as an ABC, but wasn't an ABC).

--- sbsearch/logging.py
+++ sbsearch/logging.py
@@ -4,13 +4,29 @@ import sys
 import time
 import logging
 from enum import Enum
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 import numpy as np
 from astropy.time import Time
 
 
-def setup_logger(filename: str = 'sbsearch.log', name: str = 'SBSearch',
-                 level: Optional[int] = None) -> logging.Logger:
+class ElapsedFormatter(logging.Formatter):
+    """Based on https://stackoverflow.com/questions/25194864/python-logging-time-since-start-of-program"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.t0 = time.time()
+        self.t_last = self.t0
+
+    def format(self, record):
+        record.dt0 = record.created - self.t0
+        record.dt = record.created - self.t_last
+        self.t_last = record.created
+        return super().format(record)
+
+
+def setup_logger(
+    filename: str = "sbsearch.log", name: str = "SBSearch", level: Optional[int] = None
+) -> logging.Logger:
     logger: logging.Logger = logging.getLogger(name)
 
     if level:
@@ -21,8 +37,11 @@ def setup_logger(filename: str = 'sbsearch.log', name: str = 'SBSearch',
     # reset handlers, in case already defined
     close_logger(name)
 
-    formatter: logging.Formatter = logging.Formatter(
-        '%(levelname)s %(asctime)s (' + name + '): %(message)s')
+    formatter = ElapsedFormatter(
+        "%(asctime)10s (%(dt).2f/%(dt0).2f) %(levelname)s: "
+        "[%(funcName)s] %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
 
     console: logging.StreamHandler = logging.StreamHandler(sys.stdout)
     if level is None:
@@ -39,7 +58,7 @@ def setup_logger(filename: str = 'sbsearch.log', name: str = 'SBSearch',
     return logger
 
 
-def close_logger(name: str = 'SBSearch') -> None:
+def close_logger(name: str = "SBSearch") -> None:
     """Close SBSearch loggers."""
     logger: logging.Logger = logging.getLogger(name)
     for handler in logger.handlers:
@@ -48,6 +67,41 @@ def close_logger(name: str = 'SBSearch') -> None:
 
 
 class ProgressWidget(abc.ABC):
+    def __init__(self, n: int, logger: logging.Logger) -> None:
+        self.n: int = n
+        self.logger: logging.Logger = logger
+        self.i: int = 0
+        self.t0: float = time.monotonic()
+
+    def __enter__(self):
+        self.reset()
+        return self
+
+    def __exit__(self, *args):
+        self.done()
+
+    @property
+    def dt(self):
+        return int(time.monotonic() - self.t0)
+
+    def reset(self):
+        """Reset the counter."""
+        self.i = 0
+        self.t0 = time.monotonic()
+
+    @abc.abstractmethod
+    def log(self):
+        pass
+
+    @abc.abstractmethod
+    def update(self):
+        pass
+
+    def done(self):
+        self.logger.info("{:.0f} seconds elapsed.".format(self.dt))
+
+
+class ProgressPercent(ProgressWidget):
     """Log progress as percentage.
 
 
@@ -68,44 +122,19 @@ class ProgressWidget(abc.ABC):
 
     """
 
-    def __init__(self, n: int, logger: logging.Logger) -> None:
-        self.n: int = n
-        self.logger: logging.Logger = logger
-        self.i: int = 0
-        self.t0: float = time.monotonic()
-
-    def __enter__(self):
-        self.reset()
-        return self
-
-    def __exit__(self, *args):
-        self.done()
-
-    @ property
-    def dt(self):
-        return int(time.monotonic() - self.t0)
-
-    def reset(self):
-        """Reset the counter."""
-        self.i = 0
-        self.t0 = time.monotonic()
-
     def log(self, status: float):
         """Write the current status to the log."""
-        self.logger.info('%.2f%%', status * 100)
+        self.logger.info("%.2f%%", status * 100)
 
     def update(self, increment: int = 1):
         """Increment counter and report status."""
         self.i += increment
         self.log(self.i / self.n)
 
-    def done(self):
-        self.logger.info('{:.0f} seconds elapsed.'.format(self.dt))
-
 
 class ProgressScale(Enum):
-    LOG = 'log'
-    LINEAR = 'linear'
+    LOG = "log"
+    LINEAR = "linear"
 
 
 class ProgressBar(ProgressWidget):
@@ -135,8 +164,9 @@ class ProgressBar(ProgressWidget):
 
     """
 
-    def __init__(self, n: int, logger: logging.Logger,
-                 scale: str = 'linear', length: int = 10) -> None:
+    def __init__(
+        self, n: int, logger: logging.Logger, scale: str = "linear", length: int = 10
+    ) -> None:
         super().__init__(n, logger)
         self.last_step: int = 0
         self.scale: ProgressScale = ProgressScale(scale)
@@ -145,14 +175,14 @@ class ProgressBar(ProgressWidget):
     def reset(self):
         super().reset()
         self.last_step = 0
-        self.logger.info('%d steps, %s scale', self.length, self.scale.value)
-        self.logger.info('%s', '-' * self.length)
+        self.logger.info("%d steps, %s scale", self.length, self.scale.value)
+        self.logger.info("%s", "-" * self.length)
 
     def log(self, step: int) -> None:
         self.logger.info(
-            '%s t-%d s',
-            '#' * step + '-' * (self.length - step),
-            int((time.monotonic() - self.t0) / self.i * (self.n - self.i))
+            "%s t-%d s",
+            "#" * step + "-" * (self.length - step),
+            int((time.monotonic() - self.t0) / self.i * (self.n - self.i)),
         )
 
     def update(self, increment: int = 1):
@@ -205,15 +235,15 @@ class ProgressTriangle(ProgressWidget):
         self._log: Callable
         if base == 2:
             self._log = np.log2
-            self.logger.info('Base-2 dots')
+            self.logger.info("Base-2 dots")
         elif base == 10:
             self._log = np.log10
-            self.logger.info('Base-10 dots')
+            self.logger.info("Base-10 dots")
         else:
-            raise ValueError('base must be 2 or 10')
+            raise ValueError("base must be 2 or 10")
 
     def log(self, status: int) -> None:
-        self.logger.info('%s %d', '.' * status, self.i)
+        self.logger.info("%s %d", "." * status, self.i)
 
     def update(self, increment: int = 1):
         last = self.i
