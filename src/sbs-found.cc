544abf47499550c752879d978b60557d52aab24f
Michael S. P. Kelley <msk@astro.umd.edu>
2024-09-10 11:05:26 -0400

    Change sbs-found behavior to support summaries.

--- src/sbs-found.cc
+++ src/sbs-found.cc
@@ -8,9 +8,11 @@
 #include "moving_target.h"
 #include "sbsearch.h"
 #include "sbs-cli.h"
+#include "table.h"
 
 using namespace sbsearch;
 using namespace sbsearch::cli;
+using sbsearch::table::Table;
 using std::cerr;
 using std::cout;
 using std::string;
@@ -19,10 +21,11 @@ using std::vector;
 struct Arguments
 {
     string target;
-    bool input_file;
+    string input_file;
     bool small_body;
 
     vector<string> sources; // not yet implemented
+    bool list;
     string output_filename; // not yet implemented
 
     string database;
@@ -37,17 +40,13 @@ Arguments get_arguments(int argc, char *argv[])
 
     Arguments args;
 
-    positional_options_description positional;
-    positional.add("target", 1);
-
-    options_description hidden("Hidden options");
-    hidden.add_options()("target", value<string>(&args.target), "target");
-
     options_description options("Found catalog options");
     options.add_options()(
-        "input,i", bool_switch(&args.input_file), "read target names from an input file")(
+        "target", value<string>(&args.target), "limit to this target name")(
+        "input,i", value<string>(&args.input_file), "read target names from an input file")(
         "major-body", bool_switch(&args.small_body)->default_value(true), "moving target is a major body (applies to all targets in the input file)")(
         "source,s", value<vector<string>>(&args.sources), "only show results for this source data set, may be specified multiple times")(
+        "list", bool_switch(&args.list), "list found observations")(
         "output,o", value<string>(&args.output_filename), "save the results to this file");
 
     options_description general("General options");
@@ -63,10 +62,10 @@ Arguments get_arguments(int argc, char *argv[])
     visible.add(options).add(general);
 
     options_description all("");
-    all.add(visible).add(hidden);
+    all.add(visible);
 
     variables_map vm;
-    boost::program_options::store(command_line_parser(argc, argv).options(all).positional(positional).run(), vm);
+    boost::program_options::store(command_line_parser(argc, argv).options(all).run(), vm);
     boost::program_options::notify(vm);
 
     if (vm.count("version"))
@@ -75,24 +74,72 @@ Arguments get_arguments(int argc, char *argv[])
         exit(0);
     }
 
-    if (vm.count("help") | !vm.count("target"))
+    if (vm.count("help"))
     {
         cout << R"(
-Usage: sbs-found <target> [options...]
+Usage: sbs-found [options...]
 
 Inspect the found object catalog.
-
-<target> is a moving target name or, with --input, a file
-listing multiple targets.
 )"
              << visible << "\n";
-
-        if (!vm.count("target"))
-            cout << "\ntarget is a required argument\n";
 
         exit(0);
     }
     return args;
+}
+
+void list_observations(const vector<MovingTarget> targets, SBSearch &sbs)
+{
+    Founds founds;
+    for (MovingTarget target : targets)
+    {
+        if (target.moving_target_id() == UNDEF_MOVING_TARGET_ID)
+            continue;
+
+        founds.append(sbs.db()->get_found(target));
+    }
+    cout << founds;
+}
+
+void summarize_observations(const vector<MovingTarget> targets, SBSearch &sbs)
+{
+    vector<string> names;
+    vector<double> first, last;
+    vector<int> count;
+
+    Founds founds;
+    for (MovingTarget target : targets)
+    {
+        if (target.moving_target_id() == UNDEF_MOVING_TARGET_ID)
+            continue;
+
+        founds = sbs.db()->get_found(target);
+        names.push_back(target.designation());
+        count.push_back(founds.size());
+        if (founds.size() > 0)
+        {
+            first.push_back(std::min_element(founds.begin(), founds.end(),
+                                             [](const Found &a, const Found &b)
+                                             { return a.observation.mjd_mid() < b.observation.mjd_mid(); })
+                                ->observation.mjd_mid());
+            last.push_back(std::max_element(founds.begin(), founds.end(),
+                                            [](const Found &a, const Found &b)
+                                            { return a.observation.mjd_mid() < b.observation.mjd_mid(); })
+                               ->observation.mjd_mid());
+        }
+        else
+        {
+            first.push_back(0);
+            last.push_back(0);
+        }
+    }
+
+    Table summary; // default behavior
+    summary.add_column("target", "%s", names);
+    summary.add_column("count", "%d", count);
+    summary.add_column("first", "%.6lf", first);
+    summary.add_column("last", "%.6lf", last);
+    cout << summary;
 }
 
 int main(int argc, char *argv[])
@@ -109,28 +156,23 @@ int main(int argc, char *argv[])
         SBSearch sbs(SBSearch::sqlite3, args.database, {args.log_file, log_level});
         Logger::info() << "SBSearch moving target found catalog tool." << std::endl;
 
-        vector<string> names;
-        if (args.input_file)
+        vector<MovingTarget> targets;
+        if (!args.input_file.empty())
         {
             std::ifstream input(args.target);
-            for (string line; std::getline(input, line);)
-                if ((line.size() > 0) & (line[0] != '#'))
-                    names.push_back(line);
+            for (string name; std::getline(input, name);)
+                if ((name.size() > 0) & (name[0] != '#'))
+                    targets.push_back(sbs.db()->get_moving_target(name));
         }
         else if (!args.target.empty())
-            names.push_back(args.target);
+            targets.push_back(sbs.db()->get_moving_target(args.target));
+        else
+            targets = sbs.db()->get_all_moving_targets();
 
-        Founds founds;
-        for (string name : names)
-        {
-            MovingTarget target = sbs.db()->get_moving_target(name);
-            if (target.moving_target_id() == UNDEF_MOVING_TARGET_ID)
-                continue;
-
-            founds.append(sbs.db()->get_found(target));
-        }
-
-        cout << founds;
+        if (args.list)
+            list_observations(targets, sbs);
+        else
+            summarize_observations(targets, sbs);
     }
     catch (std::exception &e)
     {
