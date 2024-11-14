bf07f33e4fd972af7c04e86571b621c6d5d334fe
Michael S. P. Kelley <msk@astro.umd.edu>
2024-11-14 14:14:37 -0500

    Add comments

--- build_s2.sh
+++ build_s2.sh
@@ -6,9 +6,13 @@ pushd
 [[ -z $S2PREFIX ]] && echo "Requires env variable S2PREFIX set to desired installation prefix" && exit 1
 [[ -z "$PYTHON_ROOT" ]] && PYTHON_ROOT=`python3 -c "import sys; print(sys.exec_prefix)"`
 
+# checkout code, S2 and Abseil
 test ! -e build && mkdir build
 cd build
 
+# install abseil from source, as per s2geometry's readme
+# (it must be configured with -DCMAKE_POSITION_INDEPENDENT_CODE=ON)
+# We are using C++11, and the last version to support it is LTS 20220623.1
 if [ ! -e $S2PREFIX/lib/libabsl_base.a ]; then
     ABSEIL_TAG=20220623.1
     test ! -e ${ABSEIL_TAG}.tar.gz && wget https://github.com/abseil/abseil-cpp/archive/refs/tags/${ABSEIL_TAG}.tar.gz
