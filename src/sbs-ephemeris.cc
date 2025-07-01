a1f77d26524693b10ab6d892168b849fd5a19f7b
Michael S. P. Kelley <msk@astro.umd.edu>
2025-07-01 19:51:59 -0400

    Try to detect and fix single step output from Horizons

--- src/sbs-ephemeris.cc
+++ src/sbs-ephemeris.cc
@@ -229,6 +229,16 @@ void add(const Arguments &args, SBSearch<DB> &sbs)
                               args.time_step,
                               args.cache);
             Ephemeris eph = Ephemeris(target, horizons.get_ephemeris_data());
+
+            // variable time step may return just one epoch, but we want at least the end points.
+            if ((eph.data().size() == 1) && (args.time_step.find("VAR") != string::npos))
+            {
+                Logger::warning() << "Variable time step detected, but only one ephemeris epoch returned.  Retrying to fetch endpoints." << endl;
+                Arguments args2 = args;
+                args2.time_step = "1";
+                return add(args2, sbs);
+            }
+
             sbs.add_ephemeris(eph);
             count += eph.num_vertices();
         }
