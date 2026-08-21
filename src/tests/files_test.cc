32f0857449f48c4522f8ef75e25a9d902bf2d8dc
Michael S. P. Kelley <msk@astro.umd.edu>
2026-08-20 20:33:48 -0400

    Use a fixture to manage logging buffer changes

--- src/tests/files_test.cc
+++ src/tests/files_test.cc
@@ -15,11 +15,35 @@
 #define DATE_PATTERN "20[0-9][0-9]-[01][0-9]-[0-3][0-9] [012][0-9]:[0-5][0-9]:[0-5][0-9]"
 
 using std::string;
+using namespace sbsearch;
 namespace fs = boost::filesystem;
 
 namespace sbsearch::testing
 {
-    TEST(FilesTests, TestReadFile)
+    class FilesTests : public ::testing::Test
+    {
+    protected:
+        static void SetUpTestSuite()
+        {
+            Logger::get_logger().attach(&capture);
+        }
+
+        static void TearDownTestSuite()
+        {
+            Logger::get_logger().reset_buffer();
+        }
+
+        void SetUp() override
+        {
+            capture.str("");
+        }
+
+        static std::stringstream capture;
+    };
+
+    std::stringstream FilesTests::capture;
+
+    TEST_F(FilesTests, TestReadFile)
     {
         std::ofstream out("sbsearch-test-file");
         out << "asdf";
@@ -29,12 +53,12 @@ namespace sbsearch::testing
         fs::remove("sbsearch-test-file");
     }
 
-    TEST(FilesTests, TestReadFileError)
+    TEST_F(FilesTests, TestReadFileError)
     {
         EXPECT_THROW(read_file("sbsearch-test-file"), std::runtime_error);
     }
 
-    TEST(FilesTests, TestCachedFileName)
+    TEST_F(FilesTests, TestCachedFileName)
     {
         const string home = std::getenv("HOME");
         setenv("HOME", "./", 1);
@@ -47,10 +71,6 @@ namespace sbsearch::testing
 
         // make the directory unwritable and test for errors
         std::string messages;
-        std::stringstream stream;
-
-        Logger &logger = Logger::get_logger();
-        logger.attach(&stream);
 
         setenv("HOME", "/tmp/sbsearch-testing", 1);
         fs::create_directory("/tmp/sbsearch-testing");
@@ -58,19 +78,21 @@ namespace sbsearch::testing
 
         fs::path fn = generate_cache_file_name("test string");
 
-        messages = stream.str();
+        messages = capture.str();
         EXPECT_TRUE(
             std::regex_search(
                 messages,
-                std::regex("20[0-9][0-9]-[01][0-9]-[0-3][0-9] [012][0-9]:[0-5][0-9]:[0-5][0-9]"
-                           "::ERROR::Could not write cache file /tmp/sbsearch-testing/sbsearch: boost::filesystem::create_directories: Permission denied.*")));
+                std::regex(DATE_PATTERN
+                           "::ERROR::Could not create cache directory "
+                           "/tmp/sbsearch-testing/.cache/sbsearch: "
+                           "boost::filesystem::create_directories: Permission denied.*")));
 
         chmod("/tmp/sbsearch-testing", S_IRUSR | S_IWUSR | S_IXUSR);
         fs::remove_all("/tmp/sbsearch-testing");
         setenv("HOME", home.c_str(), 1);
     }
 
-    TEST(FilesTests, TestWriteToCache)
+    TEST_F(FilesTests, TestWriteToCache)
     {
         if (fs::exists("/tmp/sbsearch-testing"))
             fs::remove_all("/tmp/sbsearch-testing");
@@ -87,25 +109,20 @@ namespace sbsearch::testing
 
         // make the directory unwritable and test for errors
         std::string messages;
-        std::stringstream stream;
-
-        Logger &logger = Logger::get_logger();
-        logger.attach(&stream);
 
         // cannot create file
         chmod("/tmp/sbsearch-testing", S_IRUSR | S_IWUSR | S_IXUSR);
         fs::create_directory("/tmp/sbsearch-testing/unwritable");
         chmod("/tmp/sbsearch-testing/unwritable", S_IRUSR | S_IXUSR);
         write_to_cache(fs::path("/tmp/sbsearch-testing/unwritable/cache-test"), "asdf");
-        messages = stream.str();
+        messages = capture.str();
+
         EXPECT_TRUE(
             std::regex_search(
                 messages,
                 std::regex(DATE_PATTERN "::ERROR::Could not write cache file /tmp/sbsearch-testing/unwritable/cache-test: Unable to open file for writing.*")));
-        stream.str("");
+        capture.str("");
 
         fs::remove_all("/tmp/sbsearch-testing");
-
-        logger.reset_buffer(); // i.e., detach string stream
     }
 }
